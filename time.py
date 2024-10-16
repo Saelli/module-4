01394ddd9f130fc2b801bbeff4bd1ce6b3682a82
Eleonora <eliya.kg@gmail.com>
2024-10-15 21:33:30 -0500

    Create time.py

---
+++ time.py
@@ -0,0 +1,11 @@
+#Eleonora
+# 15 October
+currentTimeStr = input("What is the current time (in hours 0-23)? ")
+waitTimeStr = input("How many hours do you want to wait ") # brackets 
+
+currentTimeInt = int(currentTimeStr)
+waitTimeInt = int(waitTimeStr)
+
+finalTimeInt = currentTimeInt + waitTimeInt
+print(finalTimeInt) 
+#typo in variables name 
