2abb5151c01974c3f76985ced47f9511920f6ab6
Eleonora <eliya.kg@gmail.com>
2024-10-15 21:33:00 -0500

    Create pirate.py

---
+++ pirate.py
@@ -0,0 +1,6 @@
+# Eleonora
+# 15 October
+greeting = input("Hello, possible pirate! What's the password?") # missing quote
+if greeting in ("Arrr!"): # change brackets
+	print("Go away, pirate.")
+else: print("Greetings, hater of pirates!") #chabge elif to else
